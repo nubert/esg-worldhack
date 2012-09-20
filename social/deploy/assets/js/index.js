f16fa372352f60ec5727d1ecb23f4e76d0e1431e
Bradley Adams <brad@bradleyadams.com>
2012-09-20 12:50:37 -0700

    More javascript suff

--- social/deploy/assets/js/index.js
+++ social/deploy/assets/js/index.js
@@ -4,20 +4,20 @@ var PARZNS = {
     	switch(type)
     	{
     		case 'sendInvite':
-    			var params = {
-    				method: 'apprequests',
-    				message: 'Come play P.A.R.Z.N.S with me!'
-    			};
-    			
-    			if(recipients) {
-    				params.to = recipients
-    			}
-    			
-    			FB.ui(params, function()
-    	        {
-    	        	console.log('Sent');
-    	        });
-    			break;
+				var params = {
+					method: 'apprequests',
+					message: 'Come play P.A.R.Z.N.S with me!'
+				};
+				
+				if(recipients) {
+					params.to = recipients
+				}
+				
+				FB.ui(params, function()
+				{
+					console.log('Sent');
+				});
+				break;
     	}
     }
 }
