fa402198a08c2e47425caafd5ce74bb34aff03ff
Steven Faccin <contactsteve@iamlost.info>
2012-09-20 14:30:34 -0700

    Adding appId, appSecret and servicesUrl to config

--- backend/lib/config.php
+++ backend/lib/config.php
@@ -1,7 +1,10 @@
-<?php 
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = 'hackathon';
-$dbName = 'parzns';
+<?php
 
-$baseUrl = 'http://hackathon.eastsidegamestudio.com/esg-worldhack/social/deploy';
+	$dbHost 		= 'localhost';
+	$dbUser 		= 'root';
+	$dbPass 		= 'hackathon';
+	$dbName 		= 'parzns';
+	$appId 			= '473854159303213';
+	$appSecret 		= '175a70ba2523c6eaff58ba2574593265';
+	$baseUrl 		= 'http://hackathon.eastsidegamestudio.com/esg-worldhack/social/deploy';
+	$serviceUrl 	= 'http://hackathon.eastsidegamestudio.com/esg-worldhack/backend/service';
