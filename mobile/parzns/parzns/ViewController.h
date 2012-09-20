0f513f3508ab3dc2635b74de44c947d827deac36
neoblargg <neoblargg@gmail.com>
2012-09-20 10:18:47 -0700

    mobile project setup

---
+++ mobile/parzns/parzns/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  parzns
+//
+//  Created by Jason Sie on 12-09-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
