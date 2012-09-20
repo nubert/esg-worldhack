4c20c1047ede428b86e549262095d651a3eed8a0
Bradley Adams <brad@bradleyadams.com>
2012-09-20 10:29:46 -0700

    Adding social swf embed and FB javascript SDK

--- social/deploy/index.php
+++ social/deploy/index.php
@@ -1,1 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<title>P.A.R.Z.N.S</title>
+	
+	<link href="/assets/css/styles.css" rel="stylesheet" type="text/css" />
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+	<script type="text/javascript">
+	var flashvars = {
+	};
+	var params = {
+	};
+	var attributes = {
+		id: "game",
+		name: "game"
+	};
+	swfobject.embedSWF(
+		"/assets/swf/Parzns.swf", 
+		"game", 
+		"760", "600", 
+		"9.0.0",
+		"/assets/swf/expressInstall.swf", 
+		flashvars, params, attributes);
+	</script>
+
+</head>
+
+<body>
+
+<div id="game">
+    <a href="http://www.adobe.com/go/getflashplayer/">
+	    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
+	</a>
+</div>
+
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '473854159303213', // App ID
+      channelUrl : '//hackathon.eastsidegamestudio.com/channel.html', // Channel File
+      frictionlessRequests: true,
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+</script>
+
+</body>
+
+</html>
