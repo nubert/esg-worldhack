45c76383da8e4a201281b63dc9ad9870b1b8f49e
Bradley Adams <brad@bradleyadams.com>
2012-09-20 17:17:01 -0700

    Phrase updates

--- backend/opengraph/phrase.php
+++ backend/opengraph/phrase.php
@@ -10,7 +10,7 @@ $letters = '';
 
 if (mysql_num_rows($result) > 0) {
 	$row = mysql_fetch_row($result);
-	$phrase = explode(" ", $row['phrase']);
+	$phrase = $row['phrase'];
 	$letters = $row['letters'];
 }
 
@@ -21,18 +21,12 @@ if ($phrase) {
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# esg_parzns: http://ogp.me/ns/fb/esg_parzns#">
   <meta property="fb:app_id" content="473854159303213" /> 
   <meta property="og:type"   content="esg_parzns:phrase" /> 
-  <meta property="og:url"    content="http://hackathon.eastsidegamestudio.com/esg-worldhack/backend/opengraph/phrase.php?letter=<?php echo $letter; ?>" /> 
-  <meta property="og:title"  content="The Letter <?php echo strtoupper($letter); ?>" /> 
+  <meta property="og:url"    content="http://hackathon.eastsidegamestudio.com/esg-worldhack/backend/opengraph/phrase.php?phrase_id=<?php echo $phrase_id; ?>" /> 
+  <meta property="og:title"  content="<?php echo $phrase; ?>" /> 
   <meta property="og:image"  content="<?php echo $baseUrl . '/assets/images/phrase.png'; ?>" />
 </head>
 <body>
-<?php
-$index = 0;
-foreach ($letters as $letter) {
-	echo '<div style="float: left;"><img src="' . $baseUrl . '/assets/images/letters/' . strtolower($letter) . '.png" style="float: left;" />' . $phrase[$index] . '</div>';
-	$index++;
-}
-?>
+<img src="<?php echo $baseUrl . '/assets/images/phrase.png'; ?>" />
 </body>
 </html>
 <?php
